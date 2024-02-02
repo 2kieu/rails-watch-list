5e2e1a94c61cff13f81c187816ccd34de2a31a7c
Thu Kieu <t.kieu@live.de>
2024-02-02 20:51:06 +0100

    change the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :lists do
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
 
